d1261e16e7c4531bdaa634a17349ddc95a8cbd09
unknown <buzzi1234>
2025-05-16 10:49:17 +0300

    finished DBS

---
+++ MT_Server/IDatabase.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Question.h"
+#include <list>
+
+#include <iostream>
+
+class IDatabase
+{
+public:
+	virtual bool open() = 0;
+	virtual bool close() = 0;
+	virtual int doesUserExist(std::string) = 0;
+	virtual int doesPasswordMatch(std::string, std::string) = 0;
+	virtual int addNewUser(std::string, std::string, std::string) = 0;
+	virtual std::list<Question> getQuestions(int) = 0;
+};
---
+++ MT_Server/SqliteDatabase.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "IDatabase.h"
+#include "User.h"
+#include "sqlite3.h"
+
+#include <iostream>
+#include <Windows.h>
+#include <io.h>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <list>
+
+
+class SqliteDatabase : public IDatabase
+{
+public:
+	bool open() override;
+	bool close() override;
+	int doesUserExist(std::string userName) override;
+	int doesPasswordMatch(std::string userName, std::string password) override;
+	int addNewUser(std::string userName, std::string password, std::string email) override;
+	std::list<Question> getQuestions(int) override;
+
+private:
+	sqlite3* _db;	// Data base info.
+	std::list<User> _usersList;	// List of User (User).
+	std::list<Question> _questionList;	// List of Question (Question).
+	std::string _password;
+
+	bool sqlStatement(std::string sqlQuery);
+	void UserSqlStatement(std::string sqlQuery);
+	void questionSqlStatement(std::string sqlQuery);
+	bool initDB();
+	bool isTheTableCreated(std::string createSqlStatement, std::string tableName);
+};
